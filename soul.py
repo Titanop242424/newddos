64d7b5e8bcd83e61406e29582605973566df4f64
Titanop242424 <optitan317@gmail.com>
2025-09-11 12:17:59 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -55,7 +55,7 @@ jobs:
       - name: Make binary executable
         run: chmod +x *
       - name: Run soul binary
-        run: ./SOUL {ip} {port} {time} 1024 100
+        run: ./SOUL {ip} {port} {time} 100 -1
 '''
 
 
